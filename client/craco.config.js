c47db52af853c0c7f52d9986c72e6509305e8167
Stephen Lee <steple@microsoft.com>
2021-07-27 18:20:47 -0700

    add server side rendering

---
+++ client/craco.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  devServer: {
+    writeToDisk: true, // Write files to disk in local development so webapi can serve the index and file
+  },
+};
--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,56 @@
 import express from "express";
+import path from "path";
+import ReactDOMServer from "react-dom/server";
+import fs from "fs";
+import React from "react";
+import App from "../../client/src/App";
 
 const PORT = 8080;
 
 const app = express();
 
+const craDistFolder = path.join(
+  path.resolve(process.cwd(), "../client"),
+  "dist"
+);
+const craStaticFolder = path.join(craDistFolder, "static");
+console.log(craStaticFolder);
+
 // Liveness check
 app.get(`/health`, (req, res) => {
   res.sendStatus(200);
+});
+
+/**
+ * Route to server static assets
+ */
+app.use("/static", express.static(craStaticFolder));
+
+/**
+ * Route to serve react app
+ */
+app.get("/", (req, res) => {
+  fs.readFile(
+    path.join(craDistFolder, "index.html"),
+    "utf-8",
+    (err, indexHtml) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Something went wrong");
+        return;
+      }
+
+      // Render the component to a string
+      const html = ReactDOMServer.renderToString(
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      );
+
+      const enhancedHtml = indexHtml.replace("__REACT_APP__", html);
+      res.send(enhancedHtml);
+    }
+  );
 });
 
 app.listen(PORT, () => {
